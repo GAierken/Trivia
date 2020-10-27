5284a03c30425fffc8371aaf8d83d174508acaec
GAierken <g.aierken@gmail.com>
2020-10-26 23:56:18 -0400

    handled json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Problems from './Apprentice_TandemFor400_Data.json'
+
 
 function App() {
+  console.log(Problems)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      {
+        Problems.map((pro) => {
+        return <div>{pro.question}</div>
+        })
+      }
     </div>
   );
 }
